fcffd69979f16c796b57eefdf9095f32fa9ae2e6
MdeFrance <michaeldefrance@gmail.com>
2019-09-26 13:47:55 +0200

    Changed function TQDM max calculate
    Changed the function to calculate the TQDM max value. Although it should be a generator expression with the right equation which still escapes me

--- Wordlist_Generator.py
+++ Wordlist_Generator.py
@@ -1,63 +1,64 @@
 import itertools
 import sys
 import time
+import os
+from tqdm.auto import tqdm  # pip install tqdm
 
- # pip install tqdm
-from tqdm.auto import tqdm
+os.system('clear')
 
-info = '''
-
-######################################################
-                By S.S.B Group
-######################################################
-
-    Suraj Singh
-    Admin
-    S.S.B Group
-    surajsinghbisht054@gmail.com
-    http://bitforestinfo.blogspot.in/
-
-    Note: We Feel Proud To Be Indian
-######################################################
-
-#######################################################################
-# High Speed Wordlist Generator : Compitable To All Operating Systems #
-#######################################################################
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-usages Example:>>
-        Please enter here all characters for combination :>> ABCabc123';./
-        Please enter here lengh of words :>> 4
-        Please enter here sequental character exclution:>> 2
-        Please enter here name of Wordlist :>> pass.txt
-**********************************************************************
-**********************************************************************
+'''
+########################################################################
+####### Wordlist Generator : Compitable For All Operating Systems ######
+########################################################################
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# usages Example:>>
+#        Please enter here all characters for combination :>> ABCabc123';./
+#        Please enter here lengh of words :>> 4
+#        Please enter here sequental character exclution:>> 2
+#        Please enter here name of Wordlist :>> pass.txt
+# **********************************************************************
+# **********************************************************************
 '''
 
-chrs = input("\n[+] Please Enter Here All Characters For Combination :> ")
+chrs = input("\n[+] Enter Here All Characters For Combination :> ")
 
-l = int(input("\n[+] Please Enter Minimum Lenth Of Words  :>  "))
+l = int(input("\n[+] Enter Minimum Lenth Of Words  :>  "))
 k = l
-j = int(input("\n[+] Please Enter Maximum Lenth Of Words  :>  "))
+j = int(input("\n[+] Enter Maximum Lenth Of Words  :>  "))
 n = j + 1
-v = int(input("\n[+] Please Enter Sequential Charachter Exclution :> "))
 
-zt = input("\n[+] Please Enter Name Of Wordlist File :> ")
+# Number that is enterd is include.
+# (ex: 2 means AA, AAA, AAAA, AAAAA, etc are exclude in you file)
+v = int(input("\n[+] Enter Sequential Charachter Exclution :> "))
 
-# This function is 'quick fix' to find out the correct iterations of tline
-def tline(k, n, chrs, v):
-  """ Returns the correct tline number. Including 'exclude sequental characters'."""
-    tline = 0
-    for i in range(k, n):
-        for xs in itertools.product(chrs, repeat=i):
-            if v > 0:
-                cnts = [sum(1 for i in grp[1])
-                        for grp in itertools.groupby(xs)]
-                if max(cnts) >= v:
-                    continue
-                tline += 1
-            else:
-                tline += 1
-    return tline
+## This function (just to come up with the right number for the progress bar despite input given by the user)
+## should be a generator expression with the right math equations to calculate that number on the fly.
+## That way it won't store anything on the memory of the computer
+
+def tline(chrs, k, n, v):
+    ''' NOW ADD THE MATH FOR THE EXCLUTION (J)'''
+    tline = []
+    for line in range(k, n):
+        if k == j:  # <--
+            # This line DOES give the correct number if k and j are equal!
+            tline.append(line**len(chrs))
+        else: # <--
+            # This line doesn't give the correct number.
+            tline.append((line**len(chrs)) - ((k - line)**len(chrs)))
+            # tline.append(len(chrs)**(((int(chrs[:n])) + 1) - line))
+        
+    return(sum(tline))
+
+
+if k == j:
+    tline=tline(chrs, k, n, v)
+    print('\n\nThis number IS correct for "total=tline" in TQDM.\ntline =', tline, "You'll see the progress bar now.")
+else:
+    tline=tline(chrs, k, n, v)
+    print('\n\nThis number is NOT correct for "total=tline" in TQDM.\ntline =', tline, "You'll NOT see the progress bar now.")
+
+
+zt=input("\n[+] Enter Name Of Wordlist File :> ")
 
 
 input('\n\n[+] Are You Ready ?\t[Press Enter]')
@@ -65,21 +66,22 @@ input('\n\n[+] Are You Ready ?\t[Press Enter]')
 
 print('\n\n++++++++++++++++++++++++ Please Wait ++++++++++++++++++++++++\n\n')
 
+# tline should might be a factorial mathematical equation.
+# tline = int(tline(k, n, chrs, v))
 
-time1 = time.asctime()
-start = time.time()
+time1=time.asctime()
+start=time.time()
 
-# If tline function needs to iterate over a lot of iterations, this will take a while ...
-loop = tqdm(total=tline(k, n, chrs, v), position=0)
+loop=tqdm(total=tline, position=0)
 
-psd = open(zt, 'a')
+psd=open(zt, 'a')
 for i in range(k, n):
-    r = i * 100 / n
-    l = str(r) + ' percent '
+    r=i * 100 / n
+    l=str(r) + ' percent '
 
     for xs in itertools.product(chrs, repeat=i):
         if v > 0:
-            cnts = [sum(1 for i in grp[1])
+            cnts=[sum(1 for i in grp[1])
                     for grp in itertools.groupby(xs)]
             if max(cnts) >= v:
                 continue
@@ -97,11 +99,11 @@ sys.stdout.write("\r\n\tDone Sucessfully")
 print('\n\n++++++++++++++++++++++++ Process Report ++++++++++++++++++++++++\n')
 
 print('\t [+] Process Started                      :   ', time1)
-end = time.time()
+end=time.time()
 print('\t [+] Process Completed                    :   ', time.asctime())
-totaltime = end - start
+totaltime=end - start
 print('\t [+] Total Time Consumed                  :   ', totaltime, 'second')
-rate = tline(k, n, chrs, v) / totaltime
+rate=tline / totaltime
 print('\t [+] Rate Of Words Generating Per Seconds :   ', rate)
 
 print('\n+++++++++++++++++++++++++ Please Wait +++++++++++++++++++++++++\n')
